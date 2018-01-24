1a41d19344d1bd876a3408118b996c0f98daa89a
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2018-01-24 09:04:57 +0100

    Initial commit

---
+++ src/main/java/hu/helixlab/Main.java
@@ -0,0 +1,48 @@
+package hu.helixlab;
+
+import hu.helixlab.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+	// Create an EntityManagerFactory when you start the application.
+	private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+			.createEntityManagerFactory("helixlab_jpa_pu");
+
+
+	public static void main(String[] args) {
+
+		EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+		EntityTransaction transaction = null;
+
+		try {
+			// Get a transaction
+			transaction = manager.getTransaction();
+			// Begin the transaction
+			transaction.begin();
+
+			// Create xy object
+			User u = new User();
+
+			// Save the xy object
+			manager.persist(u);
+
+			// Commit the transaction
+			transaction.commit();
+		} catch (Exception ex) {
+			// If there are any exceptions, roll back the changes
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			// Print the Exception
+			ex.printStackTrace();
+		} finally {
+			// Close the EntityManager
+			manager.close();
+		}
+
+	}
+}
